69a6b8a45c5aeff6528e08c48dcb3fa87e4a8b9d
Chris <chris.e.green@hotmail.com>
2016-07-04 20:59:48 +0100

    split out registers

--- src/6502.py
+++ src/6502.py
@@ -1,7 +1,27 @@
+import addressing_modes
+
+
 class 6502(object):
 
-	__init__(self, memory_controller):
+	def __init__(self, memory_controller):
+
+        self.registers = Registers()
+        self.accumulator = 0
+        self.x_index = 0
+        self.y_index = 0
+        self.sp = 0xFD
+        self.pc = 0
 
 		self.memory_controller = memory_controller
 	
-		
+	def run(self, cycles):
+
+        while cycles > 0:
+
+            opcode = self.memory_controller.read(self.pc++)
+
+            self.execute(opcode, self.registers, self.memory_controller)
+
+            cycles = cycles - self.timing_table[opcode]
+
+    def execute(self, opcode):
