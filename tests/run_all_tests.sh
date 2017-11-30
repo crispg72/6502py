7af5012d0e3bcdd9a03aa79170f9ec86e2ca3ba8
crispg72 <crispg72@users.noreply.github.com>
2017-11-30 19:41:44 +0000

    No need for script with pytest

--- tests/run_all_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=../src
-
-for file in memory_controller_tests addressing_modes_tests registers_tests opcode_tests_implied opcode_tests_loads opcode_tests_stores opcode_tests_bitshifts opcode_tests_compares opcode_tests_jumps_and_branches opcode_tests_arithmetic cpu6502_tests; do
-
-    python3 $file.py
-
-done
